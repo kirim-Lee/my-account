c9512426ebd6ea2a3a8e42031c565c9ec353d581
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2024-01-05 11:07:32 +0900

    ♻️ 인증정보api 에서 세션 공통화 사용

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
-import nextAuth, { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import prisma from '@/app/lib/client';
-import { Adapter } from 'next-auth/adapters';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_PASSWORD ?? '',
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET ?? 'candy',
-  callbacks: {
-    async session({ session, user }) {
-      session.user = user;
-      return Promise.resolve(session);
-    },
-  },
-  adapter: PrismaAdapter(prisma) as Adapter,
-};
+import nextAuth from 'next-auth';
+import { authOptions } from '@/app/lib/authOption';
 
 const handler = nextAuth(authOptions);
+
 export { handler as GET, handler as POST };
