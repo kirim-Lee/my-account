f7ac9b21b727e9e101d06e2fb1b7c9169c8615b9
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2025-03-04 17:53:26 +0900

    ✨ 총합 구현

--- app/component/organism/AccountList.tsx
+++ app/component/organism/AccountList.tsx
@@ -39,6 +39,10 @@ const AccountList = ({ accounts }: Props) => {
     dayjs(keyId).get('month')
   );
 
+  const groupedByYear = groupBy(accounts, 'endDate', (keyId) =>
+    dayjs(keyId).get('year')
+  );
+
   const wholeRange = months
     .map((month) => years.map((year) => `${year}.${month}`))
     .flat();
@@ -52,7 +56,11 @@ const AccountList = ({ accounts }: Props) => {
       <p>total</p>
       <p>tab</p>
       {'// 리스트'}
-
+      <div className=" p-2 text-right">
+        <span className="text-white bg-blue-400 rounded-full inline-block py-1 px-3">
+          {getSum(filter(accounts, (item) => !isPastYear(item.endDate)))}
+        </span>
+      </div>
       <div
         className="grid grid-flow-row-dense auto-rows-max auto-cols-max gap-2 bg-white p-10 rounded-2xl"
         style={{
@@ -100,7 +108,8 @@ const AccountList = ({ accounts }: Props) => {
             </div>
             <Show active={!((index + 1) % yearGap)}>
               <div className="text-xs text-gray-600 p-2 text-right">
-                총 {getSum(groupedByMonth.get(getMonth(index)) ?? [])} 원<br />
+                총 {getSum(groupedByMonth.get(getMonth(index)) ?? [])} 만원
+                <br />
                 지난해 제외{' '}
                 {getSum(
                   filter(
@@ -112,6 +121,35 @@ const AccountList = ({ accounts }: Props) => {
             </Show>
           </React.Fragment>
         ))}
+
+        {/** total by year */}
+        <div
+          className="border-b border-b-gray-100"
+          style={getGridColSpan(yearGap + 2)}
+        />
+        <div>
+          <h4 className="rounded-full text-blue-600 text-sm border-blue-400 bg-blue-50 border text-center h-10 w-10 py-2.5">
+            합계
+          </h4>
+        </div>
+        {years.map((year) => (
+          <div className="text-xs text-gray-600 p-2 " key={year}>
+            {year}:{' '}
+            <span className="text-sm text-orange-500">
+              {getSum(groupedByYear.get(year) ?? [])}
+            </span>{' '}
+            만원
+          </div>
+        ))}
+        <div className="text-xs text-gray-600 p-2 text-right">
+          총 <span className="text-sm text-orange-500">{getSum(accounts)}</span>{' '}
+          만원
+          <br />
+          지난해 제외{' '}
+          <span className="text-gray-600">
+            {getSum(filter(accounts, (item) => !isPastYear(item.endDate)))}
+          </span>
+        </div>
       </div>
     </div>
   );
